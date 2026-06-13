095741f044855bf49d876e333c95dc51a1522918
Shlok Warukar <warulkarshlok@gmail.com>
2026-06-13 16:36:39 +0000

    STrtings ADDEr

---
+++ 415Add_strings.java
@@ -0,0 +1,11 @@
+import java.math.BigInteger;
+class Solution {
+    public String addStrings(String num1, String num2) {
+        BigInteger a = new BigInteger(num1);
+        BigInteger b = new BigInteger(num2);
+
+        BigInteger ans = a.add(b);
+        String num3 = String.valueOf(ans);
+        return num3;
+    }
+}
