1bb9c7be7df2b6d3fc89982014cfca82e8f698ac
Shlok Warukar <warulkarshlok@gmail.com>
2026-06-21 18:22:53 +0000

    2-D Array

---
+++ 1351. Count Negative Numbers in a Sorted Matrix.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countNegatives(int[][] grid) {
+        int count = 0 ;
+        for (int[] ints : grid) {
+            for (int j = 0; j < ints.length; j++) {
+                if (ints[j] < 0) {
+                    count++;
+                }
+
+            }
+
+        }
+        return count ;
+    }  
+    }
